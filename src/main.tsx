e9af7d6e90572785acfa5121e880899d12661f5a
tsubasa-km <mc0627tsubasa283@gmail.com>
2025-09-01 11:23:11 +0900

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home.tsx";
+import Admin from "./pages/Admin.tsx";
+import Setting from "./pages/Setting.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/setting" element={<Setting />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
